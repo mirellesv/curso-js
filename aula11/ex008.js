e30ea5e6c1fa5f2db33b3864d0fe1d96e51419b6
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2023-12-29 20:54:14 -0300

    condicao simples

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 80
+
+console.log(`Você está dirigindo a ${velocidade} km/h.`)
+
+if(velocidade > 60){ // CONDIÇÂO SIMPLES
+    console.log(`Você ultrapassou o limite de velocidade permitido. MULTADO!`)
+}
