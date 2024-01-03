8283acaa9f77222fc74db12f05098cb55b4392d3
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2024-01-03 16:41:48 -0300

    segunda parte do exercício 12

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,24 @@
-alert('Olá!')
+function carregar(){
+    var mensagem = window.document.getElementById("msg")
+    var imagem = window.document.getElementById("foto")
+    var agora = new Date()
+    var horas = agora.getHours()
+
+    horas = 19
+
+    mensagem.innerHTML = `Agora são ${horas} horas. `
+
+    if(horas >= 0 && horas < 12){
+        mensagem.innerHTML += ('Bom dia!')
+        imagem.src = 'manha.jpg'
+        document.body.style.background = '#fde781'
+    }else if(horas < 18){
+        mensagem.innerHTML += ('Boa tarde!')
+        imagem.src = 'tarde.jpg'
+        document.body.style.background = '#909fb2'
+    }else{
+        mensagem.innerHTML += ('Boa noite!')
+        imagem.src = 'noite.jpg'
+        document.body.style.background = '#05142b'
+    }
+}
