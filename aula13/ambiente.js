fcaa1eb0ba04c098cd3de3b4c3ff5fa199941ada
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2024-01-05 23:14:25 -0300

    repetições

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,13 +1,15 @@
 var cont = 1
 
-/*
+
 while(cont <= 10){
     console.log(`Passo ${cont}`)
     cont++
 }
-*/
+
 
 do{
     console.log(`Passo ${cont}`)
     cont++
 }while(cont <= 6)
+
+
---
+++ aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Começando...')
+for (var cont = 1; cont <= 4; cont++){
+    console.log(`Passo ${cont}`)
+}
+console.log('FIM!')
