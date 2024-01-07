4ad386a589a448f1f894796cfa20f9f5aafe7d9c
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2024-01-07 19:57:48 -0300

    tabuada melhorada

--- aula14ex/ex016/teste.js
+++
@@ -1,5 +0,0 @@
-var incremento = 1
-
-for(var i = 10; i >= 1; i = i - incremento){
-    console.log(i)
-}
--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,16 +1,19 @@
 function mostrar_tabuada(){
     var txtnum = document.querySelector('input#txtnum')
-    var num = txtnum.value
-    var res_tab = document.querySelector('div#res_tab')
-
-    res_tab.innerHTML = ''
+    var tab = document.querySelector('select#seltab')
 
     if(txtnum.value.length == 0){
         window.alert('Por favor, insira um número.')
     }else{
+        var num = Number(txtnum.value)
+        tab.innerHTML = ''
+
         for(var i = 1; i <= 10; i++){
-            res_tab.innerHTML += num + ' x ' + i + ' = ' + num*i
-            res_tab.innerHTML += '<br>'
+            var item = document.createElement('option')
+
+            item.text = `${num} x ${i} = ${num*i}`
+            item.value = `tab${i}`
+            tab.appendChild(item) 
         }
     }
 }
