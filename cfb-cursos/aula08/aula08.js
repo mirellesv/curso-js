7cf3717992f4b4fe4df444ca281182fc9dfcfd84
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2024-01-11 17:50:27 -0300

    operadores bitwise

---
+++ cfb-cursos/aula08/aula08.js
@@ -0,0 +1,16 @@
+// Operadores bitwise (&, | e ^)
+
+let num1 = 10
+let num2 = 11
+
+console.log(num1 & num2)
+console.log(num1 | num2)
+console.log(num1 ^ num2)
+
+// Deslocamento à esquerda e à direita
+
+let num3 = 5
+let num4 = 20
+
+console.log(num3 << 1)
+console.log(num4 >> 2)
