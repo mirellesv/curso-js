9f6ed63cb2155396933434f2216ed52650c0c06a
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2024-01-12 21:27:33 -0300

    diferenças entre FOR IN e FOR OF

---
+++ cfb-cursos/aula16/aula16.js
@@ -0,0 +1,27 @@
+let num = [10, 20, 30, 40, 50]
+
+// for(let i = 0; i < num.length; i++){
+//     console.log(num[i])
+// }
+
+// FOR IN -> RETORNA AS POSIÇÕES
+
+for (let i in num){
+     console.log(num[i])
+}
+
+// FOR OF -> RETORNA OS ELEMENTOS
+
+// for (let i of num){
+//     console.log(i)
+// }
+
+// obj = window.document.getElementsByTagName('div')
+
+// for(let i in obj){
+//     console.log(i)
+// }
+
+// for(let i of obj){
+//     i.innerHTML = "Curso"
+// }
