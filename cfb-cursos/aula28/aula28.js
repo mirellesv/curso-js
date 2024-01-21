f723f26172f5325fe2897239a1579c9a086ba494
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2024-01-21 16:16:07 -0300

    método map

--- cfb-cursos/aula28/aula28.js
+++ cfb-cursos/aula28/aula28.js
@@ -1,5 +1,28 @@
-const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'React']
+// ---------------- PERCORRENDO ELEMENTOS DE UM ARRAY ----------------
 
-cursos.map((el, i)=>{
-    console.log('Curso: ' + el + ' - Posição do curso: ' + i)
+cursos = ['HTML', 'CSS', 'JavaScript', 'React']
+
+const percorrer = cursos.map((elemento, indice) =>{
+    return "<div>" + elemento + "</div>"
 })
+
+console.log(percorrer)
+
+// ---------------- DOBRAR ELEMENTOS DE UM ARRAY ----------------
+
+let array1 = [1, 2, 3, 4]
+
+let array2 = array1.map((elemento) => elemento * 2)
+
+console.log(array2)
+
+// ---------------- CONVERTER DE CELSIUS PARA FAHRENHEIT ----------------
+let temperaturasF = [0, 32, 45, 50, 60, 100]
+
+function converter(x){
+    return Math.round(((x - 32) * (5/9)))
+}
+
+let temperaturasC = temperaturasF.map(converter)
+
+console.log(temperaturasC)
