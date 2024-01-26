3ead22666e4e10b0cce1bb39ae8f0e99749d41b7
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2024-01-26 12:22:46 -0300

    querySelector e querySelectorAll

---
+++ cfb-cursos/aula33/aula33.js
@@ -0,0 +1,9 @@
+const query_filhos = document.querySelectorAll('div > p')
+const query_classe = document.querySelectorAll('.c1')
+const query_elemento = document.querySelector('div#c1')
+const query_classes = document.querySelectorAll('.c1, p')
+
+console.log(query_filhos)
+console.log(query_classe)
+console.log(query_elemento)
+console.log(query_classes)
