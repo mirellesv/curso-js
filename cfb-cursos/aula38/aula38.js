9daeb2c2354085192f979d3ceff44364b7ebb317
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2024-01-31 16:52:12 -0300

    relação dos elementos no DOM

---
+++ cfb-cursos/aula38/aula38.js
@@ -0,0 +1,9 @@
+const caixa = document.getElementById('caixa1')
+const botao_1 = document.getElementById('c1')
+
+console.log(botao_1.parentNode)
+console.log(caixa.getRootNode())
+console.log(caixa.ownerDocument)
+console.log(caixa.children[caixa.children.length - 1])
+console.log(caixa.firstElementChild)
+console.log(caixa.)
