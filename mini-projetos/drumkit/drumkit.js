4e6b05e400bb2716905a93c5ac77ddbd54d52149
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2024-06-21 18:10:57 -0300

    projeto drumkit finalizado

--- mini-projetos/drumkit/drumkit.js
+++ mini-projetos/drumkit/drumkit.js
@@ -1,14 +1,14 @@
 const container_teclas = document.querySelector('#container-teclas')
 const sons = [
-    {id: "0", som: "boom.wav"},
-    {id: "1", som: "clap.wav"},
-    {id: "2", som: "hihat.wav"},
-    {id: "3", som: "kick.wav"},
-    {id: "4", som: "openhat.wav"},
-    {id: "5", som: "ride.wav"},
-    {id: "6", som: "snare.wav"},
-    {id: "7", som: "tink.wav"},
-    {id: "8", som: "tom.wav"},
+    { id: "0", som: "boom.wav" },
+    { id: "1", som: "clap.wav" },
+    { id: "2", som: "hihat.wav" },
+    { id: "3", som: "kick.wav" },
+    { id: "4", som: "openhat.wav" },
+    { id: "5", som: "ride.wav" },
+    { id: "6", som: "snare.wav" },
+    { id: "7", som: "tink.wav" },
+    { id: "8", som: "tom.wav" },
 ]
 const teclas = ["A", "S", "D", "F", "G", "H", "J", "K", "L"]
 
@@ -26,25 +26,32 @@ const criar_teclas = () => {
 
 criar_teclas()
 
-function tocar_som(tecla_escolhida){
+function tocar_som(tecla_escolhida) {
     // Encontrando o som associado a tecla escolhida
     const indice_associado = teclas.indexOf(tecla_escolhida)
-    if(Number(teclas.indexOf(tecla_escolhida)) !== -1){ // Garantia de que o som somente irá tocar quando uma tecla for escolhida (e não quando outro lugar na div_container for clicado)
-        const som_escolhido = new Audio(`./sounds/${sons[indice_associado].som}`)
+    const div = document.querySelector(`#tecla_${tecla_escolhida}`)
+    div.classList.add('click')
+    window.setTimeout(function(){ // A classe click é adicionada e depois removida depois de 0.6 s
+        div.classList.remove('click')
+    }, 600)
+    // div.classList.remove('click')
+    console.log(div)
+        if (Number(teclas.indexOf(tecla_escolhida)) !== -1) { // Garantia de que o som somente irá tocar quando uma tecla for escolhida (e não quando outro lugar na div_container for clicado)
+            const som_escolhido = new Audio(`./sounds/${sons[indice_associado].som}`)
 
-        som_escolhido.play()
-    }
+            som_escolhido.play()
+        }
 }
 
-function tocar_som_click(evt){
+function tocar_som_click(evt) {
     const tecla_pressionada = evt.target.outerText
-   
+
     tocar_som(tecla_pressionada)
 }
 
-function tocar_som_teclado(evt){
+function tocar_som_teclado(evt) {
     const tecla_digitada = evt.key.toUpperCase()
-    
+
     tocar_som(tecla_digitada)
 }
 
