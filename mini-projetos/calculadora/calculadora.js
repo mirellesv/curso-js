cd9c6f5d591593b61560ecbd739b92c1a7f6a012
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2024-12-27 13:55:39 -0300

    inclusao de operadores unarios

--- mini-projetos/calculadora/calculadora.js
+++ mini-projetos/calculadora/calculadora.js
@@ -13,13 +13,17 @@ function clicou_botao(e){
 
     console.log(texto)
 
-    let operador_unario = texto == "CE" || texto == "C" || texto == "A" ? true : false
+    let operador_unario = texto == "CE" || texto == "C" || texto == "A" || texto == "±" || texto == "," ? true : false
 
     if (operador_unario == false){
         if(!isNaN(texto) && (cliques == 0 || cliques == 1)){
             visor.innerHTML += `${texto}`
             if(cliques == 0){
-                operador1 = Number(visor.innerHTML)
+                if(visor.innerHTML.includes(",")){
+                    operador1 = Number(visor.innerHTML.replace(",", "."))
+                }else{
+                    operador1 = Number(visor.innerHTML)
+                }
             }else if(cliques == 1){
                 // A string do visor é dividida em três e é obtido o segundo operador
                 const textoOperador2 = visor.innerHTML.split(" ")[2]
@@ -57,5 +61,52 @@ function clicou_botao(e){
                 visor.innerHTML = `${resultado}`
             }
         }
-    }  
+    }else{
+        switch(texto){
+            case 'CE':
+                if(operador1 != 0){
+                    if(operador2 != 0){
+                        operador2 = 0
+                        visor.innerHTML = visor.innerHTML.replace(visor.innerHTML.split(" ")[2], "")
+                    }else{
+                        operador1 = 0
+                        visor.innerHTML = ``
+                    }
+                }
+                break
+            case 'A':
+                // Remove o último dígito inserido
+                const novoTexto = visor.innerHTML.slice(0, -1)
+                visor.innerHTML = novoTexto
+                break
+            case 'C':
+                // Apaga todos os dígitos inseridos
+                visor.innerHTML = ``
+                operador1 = 0
+                operador2 = 0
+                break
+            case '±':
+                if(operador1 != 0){
+                    if(operador2 != 0){
+                        operador2 = operador2 * (-1)
+                        visor.innerHTML = visor.innerHTML.split(" ")[0] + visor.innerHTML.split(" ")[1] + " (-" + visor.innerHTML.split(" ")[2] + ")"
+                    }else{
+                        operador1 = operador1 * (-1)
+                        visor.innerHTML = "-" + visor.innerHTML
+                    }
+
+                    console.log("Operador 1: ", operador1)
+                    console.log("Operador 2: ", operador2 )
+                }
+                break
+            case ',':
+                visor.innerHTML = visor.innerHTML + ","
+
+                console.log("Operador 1", operador1)
+                console.log("Operador 2:", operador2)
+                break
+            default:
+                resultado = 0
+        } 
+    } 
 }
